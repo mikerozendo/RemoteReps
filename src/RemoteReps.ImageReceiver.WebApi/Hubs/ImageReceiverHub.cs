c9b218074f3552fad525352ee069d574bb173578
mikerozendo <mikerozendo@gmail.com>
2024-10-21 01:33:05 -0300

    feat:creating empty consoleapp to acts as the RTSP listener

--- src/RemoteReps.ImageReceiver.WebApi/Hubs/ImageReceiverHub.cs
+++ src/RemoteReps.ImageReceiver.WebApi/Hubs/ImageReceiverHub.cs
@@ -4,5 +4,8 @@ namespace RemoteReps.ImageReceiver.WebApi.Hubs;
 
 public sealed class ImageReceiverHub : Hub
 {
-    
+    // public override async Task OnConnectedAsync()
+    // {
+    //     return await OnConnectedAsync();
+    // }
 }
