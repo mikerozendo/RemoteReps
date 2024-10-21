9a2e9edebf0009e4408e3afaa8a9f5ea407099ca
mikerozendo <mikerozendo@gmail.com>
2024-10-21 11:29:15 -0300

    feat: setting up app to read configuration.json file in order to get RtspSourceUrl

--- src/RemoteReps.RTSPListener.Console/Program.cs
+++ src/RemoteReps.RTSPListener.Console/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LibVLCSharp.Shared;
+using Microsoft.Extensions.Configuration;
+
+var configurationBuilder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("configuration.json", optional: false)
+    .Build();
+
+var rtspSourceUrl = configurationBuilder.GetSection("RtspSourceUrl").Value;
+ArgumentNullException.ThrowIfNull(rtspSourceUrl, nameof(rtspSourceUrl));
+await StartRtspClient(rtspSourceUrl);
+
+
+async Task StartRtspClient(string rtspSourceUrl)
+{
+    Core.Initialize();
+    
+    using var libVlc = new LibVLC();
+    using var mediaPlayer = new MediaPlayer(libVlc);
+    using var media = new Media(libVlc, rtspSourceUrl, FromType.FromLocation);
+
+    mediaPlayer.Media = media;
+    mediaPlayer.EncounteredError += (sender, e) =>
+    {
+        Console.WriteLine("An error has occurred while trying to download the RTSP stream.");
+    };
+
+    mediaPlayer.Playing += (sender, e) => { Console.WriteLine("Starting to download the RTSP stream."); };
+    mediaPlayer.Stopped += (sender, e) => { Console.WriteLine("Stream download has stopped."); };
+
+    mediaPlayer.Play();
+
+    Console.WriteLine("Press enter to stop");
+    Console.ReadLine();
+
+    mediaPlayer.Stop();
+    await Task.CompletedTask;
+}
+
