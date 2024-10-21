a7bca719e9ca2e7f34e329cd81b24f4da2577cc9
mikerozendo <mikerozendo@gmail.com>
2024-10-21 01:27:25 -0300

    feat:creating empty webapi project

---
+++ src/RemoteReps.ImageReceiver.WebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RemoteReps.ImageReceiver.WebApi.Controllers;
+
+[ApiController]
+[Route("api/health-check")]
+public sealed class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Check() => Ok();
+}
